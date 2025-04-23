2ff7fb2ee25260040e57d9343292da8e687996af
Muhammad Salman <salmantayyab41@gmail.com>
2025-04-23 11:20:36 +0500

    privacy policy added

--- app/src/main/java/com/systemnox/pdfly/constants/Constants.kt
+++ app/src/main/java/com/systemnox/pdfly/constants/Constants.kt
@@ -5,7 +5,7 @@ class Constants {
     companion object {
 
         val contactEmail = "salmantayyab41@gmail.com"
-        val sourceCode = "https://google.com"
+        val sourceCode = "https://salman440.github.io/pdfly-privacy-policy/"
         val privacypolicy = "https://google.com"
         val appVersion = "1.0"
         val companyName = "SystemNox"
